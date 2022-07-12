17d54436e822d97a8c6f467e5e6e716f5e7c511e
Your Name <you@example.com>
2022-07-12 23:24:15 +0530

    Added toasted

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { addTodo,deleteTodo,removeTodo } from '../action'
 import { useDispatch,useSelector } from 'react-redux'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+
+
 export const Todo = () => {
   const[inputdata,setInputdata]=useState("")
   const [disable, setDisable] = useState(false)
   const [time, setTime] = useState(10)
+  const [toaster, setToaster] = useState(false)
+  
 
   const list=useSelector((state)=>state.todoreducer.list)
   const dispatch=useDispatch()
 
+  
   useEffect(()=>{
     countDown()
+    
   },[time])
 
   const countDown = () => {
@@ -23,25 +32,47 @@ export const Todo = () => {
       },1000)
 
       if(time == 0){
-          clearTimeout(id)
+           clearTimeout(id)
+            toast("Time Up")
+          
       }
 
 
   }
 
+  
+
 
   setTimeout(()=>{
     setDisable(true)
   },10000)
-
+  
+  const clicked = () => {
+    dispatch(addTodo(inputdata),setInputdata("") )
+    setToaster(true)
+    toast("Task Added")
+    
+  }
+  
   return (
     <div>
-        {time}
+
+        <div>
+           
+            <ToastContainer />
+           
+        </div>
+
+
+
+
+        <h3>Countdown:{time}</h3>
+         
          <input disabled={disable} onChange={(e)=>{
           setInputdata(e.target.value)
          }} type="text" value={inputdata} />
 
-         <button disabled={disable} onClick={()=>dispatch(addTodo(inputdata),setInputdata(""))}
+         <button disabled={disable} onClick={clicked}
          
          
          
